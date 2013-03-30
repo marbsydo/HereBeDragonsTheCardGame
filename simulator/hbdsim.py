34c4ad08bb1f679e7886b8dfed859dc708f97b24
Muffin <muffin@CPE1704TKS.local>
2013-03-30 20:58:31 +0000

    print map

--- simulator/hbdsim.py
+++ simulator/hbdsim.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+
+import sys
+
 print "Here be Dragons: The Card Game: The Simulator\n"
 
 def enum(*sequential, **named):
@@ -16,7 +19,7 @@ class Map:
 	def __init__(self, width, height):
 		self.width = width
 		self.height = height
-		self.tile = {(x,y):0 for x in range(8) for y in range (8)}
+		self.tile = {(x,y):SquareType.OpenOcean for x in range(8) for y in range (8)}
 
 	def GetTile(self, x, y):
 		return self.tile[(x, y)]
@@ -24,10 +27,20 @@ class Map:
 	def SetTile(self, x, y, v):
 		self.tile[(x, y)] = v
 
+	def Print(self):
+		for x in range(0, self.width - 1):
+			for y in range(0, self.height - 1):
+				sys.stdout.write('X')
+				#print('X'),
+			sys.stdout.write('\n')
+			#print('\n'),
+
 class Card:
 	def __init__(self, cardType):
 		self.type = cardType
 
 map = Map(8,8)
-map.SetTile(4, 4, 7)
+map.SetTile(4, 4, SquareType.TreasureIsland)
 print map.GetTile(4, 4)
+
+map.Print()
