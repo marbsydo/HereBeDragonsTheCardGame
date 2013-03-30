794152fd3020a7b86d3c9f203e9328ece91ecf9e
Muffin <muffin@CPE1704TKS.local>
2013-03-30 20:16:12 +0000

    Map class

--- simulator/hbdsim.py
+++ simulator/hbdsim.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python
-print "Here be Dragons: The Card Game: The Simulator"
+print "Here be Dragons: The Card Game: The Simulator\n"
+
+class Map:
+	def __init__(self, width, height):
+		self.width = width;
+		self.height = height;
+
+x = Map(8,8)
+print x.width
