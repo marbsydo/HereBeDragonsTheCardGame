0f08f031f153e1def398597a26c166f8647f0c20
Muffin <muffin@CPE1704TKS.local>
2013-03-30 20:27:55 +0000

    Added enums

--- simulator/hbdsim.py
+++ simulator/hbdsim.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 print "Here be Dragons: The Card Game: The Simulator\n"
 
+def enum(*sequential, **named):
+	enums = dict(zip(sequential, range(len(sequential))), **named)
+	return type('Enum', (), enums)
+
+CardType = enum('OpenOcean', 'TreasureIsland', 'Town', 'Unexplored', 'Whirlpool', 'Whirlwind', 'Storm', 'Shipwreck')
+
 class Map:
 	def __init__(self, width, height):
 		self.width = width;
 		self.height = height;
 
-x = Map(8,8)
-print x.width
+class Card:
+	def __init__(self, cardType):
+		self.type = cardType;
+
+map = Map(8,8)
