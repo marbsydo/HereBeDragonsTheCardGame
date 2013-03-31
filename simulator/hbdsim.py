a4790b24eb0c9e1f56f309ddd744919267b2b18f
Muffin <muffin@CPE1704TKS.local>
2013-03-31 00:26:28 +0000

    basic map generation

--- simulator/hbdsim.py
+++ simulator/hbdsim.py
@@ -8,39 +8,54 @@ def enum(*sequential, **named):
 	enums = dict(zip(sequential, range(len(sequential))), **named)
 	return type('Enum', (), enums)
 
-SquareType = enum('OpenOcean', 'TreasureIsland', 'Town', 'Unexplored', 'Whirlpool', 'Whirlwind', 'Storm', 'Shipwreck')
-'''
-discoveryPile = List();
-discoveryDiscardPile = List();
-treasuresPile = List();
-treasuresDiscardPile = List();
-'''
+TileType = enum('Unexplored', 'Town' ,'OpenOcean', 'TreasureIsland', 'Whirlpool', 'Whirlwind', 'Storm', 'Shipwreck')
+
+class Tile:
+	def __init__(self, tileType):
+		self.tileType = tileType
+
+	def GetChar(self):
+		return {
+			TileType.Unexplored: ' ',
+			TileType.Town: 'T',
+			TileType.OpenOcean: '~',
+			TileType.TreasureIsland: 'X',
+			TileType.Whirlpool: 'o',
+			TileType.Whirlwind: '*',
+			TileType.Storm: '^',
+			TileType.Shipwreck: '&',
+		}.get(self.tileType, '?')
+
+
 class Map:
 	def __init__(self, width, height):
 		self.width = width
 		self.height = height
-		self.tile = {(x,y):SquareType.OpenOcean for x in range(8) for y in range (8)}
+		self.tiles = {(x,y):Tile(TileType.OpenOcean) for x in range(width) for y in range (height)}
 
 	def GetTile(self, x, y):
-		return self.tile[(x, y)]
+		return self.tiles[(x, y)]
 
-	def SetTile(self, x, y, v):
-		self.tile[(x, y)] = v
+	def SetTile(self, x, y, tile):
+		self.tiles[(x, y)] = tile
 
 	def Print(self):
-		for x in range(0, self.width - 1):
-			for y in range(0, self.height - 1):
-				sys.stdout.write('X')
-				#print('X'),
+		for x in range(0, self.width):
+			for y in range(0, self.height):
+				tile = self.GetTile(x, y)
+				tileChar = tile.GetChar()
+				sys.stdout.write(tileChar)
 			sys.stdout.write('\n')
-			#print('\n'),
+
+
 
 class Card:
 	def __init__(self, cardType):
 		self.type = cardType
 
-map = Map(8,8)
-map.SetTile(4, 4, SquareType.TreasureIsland)
-print map.GetTile(4, 4)
-
+map = Map(8, 8)
+map.SetTile(0, 0, Tile(TileType.Town))
+map.SetTile(0, 7, Tile(TileType.Town))
+map.SetTile(7, 0, Tile(TileType.Town))
+map.SetTile(7, 7, Tile(TileType.Town))
 map.Print()
